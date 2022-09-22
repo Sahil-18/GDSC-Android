e317f3c31497e5171aec01cbb426088d5340883e
Sahil-18 <sahilpurohit18@gmail.com>
2022-09-22 10:06:15 +0530

    Updated styling

--- ToDo List App/app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ ToDo List App/app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.todolistapp.ui.theme.ToDoListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,25 +35,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ToDoItem(toDo: String, onValueChange: (String) -> Unit, onButtonClick: () -> Unit) {
-    Row {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, top = 20.dp, end = 10.dp, bottom = 40.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         TextField(
             value = toDo,
             onValueChange = onValueChange,
             label = {Text("Add to do item")},
-            singleLine = true
+            singleLine = true,
+            modifier = Modifier
+                .height(60.dp)
+                .width(200.dp)
         )
-        Button(onClick = onButtonClick) {
-            Text(text = "Add item")
+        Spacer(modifier = Modifier.padding(20.dp))
+        Button(
+            onClick = onButtonClick,
+            modifier = Modifier
+                .height(40.dp)
+                .width(70.dp),
+        ) {
+            Text(text = "Add")
         }
     }
 }
 
 @Composable
 fun ToDoListOneItem(value : String, onButtonClick: (String) -> Unit){
-    Row() {
-        Text(text = value)
-        Button(onClick = { onButtonClick(value) }) {
-            Text(text = "Delete this item")
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, top = 20.dp, end = 10.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Top
+    ) {
+        Text(
+            text = value,
+            modifier = Modifier
+                .height(40.dp)
+                .width(200.dp)
+        )
+        Button(
+            onClick = { onButtonClick(value) },
+            modifier = Modifier
+                .height(40.dp)
+                .width(90.dp)
+        ) {
+            Text(text = "Delete")
         }
     }
 }
@@ -61,7 +95,20 @@ fun ToDoItemList(toDoList : ArrayList<String>, onClick: (String) -> Unit) {
 //        toDoList.remove(it)
 //    }
 
-    Column() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(top = 20.dp, start = 40.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Text(
+            text = "To Do items",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue
+        )
         for(item in toDoList){
             ToDoListOneItem(value = item,
                 onButtonClick = onClick
@@ -78,7 +125,14 @@ fun ToDoList() {
     val onClick: (value:String) -> Unit = {
         toDoList.remove(it)
     }
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
         ToDoItem(toDo = toDo,
             onValueChange = {toDo = it},
             onButtonClick = {
